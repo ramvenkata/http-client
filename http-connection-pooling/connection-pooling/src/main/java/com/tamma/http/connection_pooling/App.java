35a1f233303fc3f8bda1f4f3bb8efce86ae7de95
Ramesh Tamma <ramesh.tamma@hotmail.com>
2019-11-05 18:06:47 -0800

    Apache http-client connection pooling module.

---
+++ http-connection-pooling/connection-pooling/src/main/java/com/tamma/http/connection_pooling/App.java
@@ -0,0 +1,13 @@
+package com.tamma.http.connection_pooling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
